6995f6022969cf03730feab45932bcc95dee3d75
Mika Vainio <koti.mika.vainio@gmail.com>
2024-11-13 14:08:38 +0200

    Esimerkkejä Wiki-sivuja varten

---
+++ UiFromConverted.py
@@ -0,0 +1,34 @@
+# PYSIDE6-MALLINE SOVELLUKSEN PÄÄIKKUNAN LUOMISEEN
+# KÄÄNNETYSTÄ KÄYTTÖLIITTYMÄTIEDOSTOSTA (MainWindow.py)
+# =====================================================
+
+# KIRJASTOJEN JA MODUULIEN LATAUKSET
+# ----------------------------------
+import os # Polkumääritykset
+import sys # Käynnistysargumentit
+
+from PySide6 import QtWidgets # Qt-vimpaimet
+from MainWindow import Ui_MainWindow # Käännetyn käyttöliittymän luokka
+
+# Määritellään luokka, joka perii QMainWindow- ja Ui_MainWindow-luokan
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    """A class for creating main window for the application"""
+    
+    # Määritellään olionmuodostin ja kutsutaan yliluokkien muodostimia
+    def __init__(self):
+        super().__init__()
+
+        # Kutsutaa käyttöliittymän muodostusmetodia setupUi
+        self.setupUi(self)
+
+# Luodaan sovellus
+app = QtWidgets.QApplication(sys.argv)
+
+# Luodaan objekti pääikkunalle ja tehdään siitä näkyvä
+window = MainWindow()
+window.show()
+
+# Käynnistetään sovellus ja tapahtumienkäsittelijä
+app.exec()
+
+    
