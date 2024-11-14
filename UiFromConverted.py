2d714cc0b33f87dd03fb618f149b4fef0ea34333
Mika Vainio <koti.mika.vainio@gmail.com>
2024-11-14 15:04:26 +0200

    Lisätty kommetti

--- UiFromConverted.py
+++ UiFromConverted.py
@@ -27,8 +27,10 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # OHJELMOIDUT SIGNAALIT
         # ---------------------
         
-        # Kun Tulosta painiketta on klikattu, kutsutaan updatePrintedLabel-metodia
+        # Kun Tulosta-painiketta on klikattu, kutsutaan updatePrintedLabel-metodia
         self.ui.tulostaPushButton.clicked.connect(self.updatePrintedLabel)
+
+        # Kun Vaara-painiketta on klikattu, kutsutaan openWarning-metodia
         self.ui.varoitaPushButton.clicked.connect(self.openWarning)
    
    
