88cd1ac105ed479430dc6798ce97646873990c7d
Mika Vainio <koti.mika.vainio@gmail.com>
2024-11-20 15:30:14 +0200

    Lisätty virheilmoitus ominaisuuden testit

--- tests/test_identityCheck2.py
+++ tests/test_identityCheck2.py
@@ -88,3 +88,16 @@ def test_splitSsn():
 def test_getDateOfBirth():
     testSsnOK.getDateOfBirth()
     assert testSsnOK.dateOfBirth == '1928-07-13'
+
+# Testataan, että isValid-metodi antaa oikeat virheilmoitukset
+def test_ssnShortError(): # Liian lyhyt hetu
+    testSsnShort.isValidSsn()
+    assert testSsnShort.errorMessage == 'Henkilötunnuksesta puuttuu merkkejä'
+
+def test_ssnLongError(): # Liian pitkä hetu
+    testSsnLong.isValidSsn()
+    assert testSsnLong.errorMessage == 'Henkilötunnuksessa ylimääräisiä merkkejä'
+
+def test_ssnInvalidError(): # Muu virhe
+    testSsnWrongCheckSum.isValidSsn()
+    assert testSsnWrongCheckSum.errorMessage == 'Syötetty henkilötunnus ei vastaa varmistussummaa'
